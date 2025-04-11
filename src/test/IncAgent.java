7a2d786c3ab65741319794ebeaf79341fc2a2a4d
Ido Sar Shalom <idosarshalom@gmail.com>
2025-04-11 23:40:40 +0300

    feat: add Plus and Inc agents.

---
+++ src/test/IncAgent.java
@@ -0,0 +1,80 @@
+package test;
+
+import java.util.Arrays;
+
+public class IncAgent implements Agent {
+
+    private final String[] subscribedTopics;
+    private final String[] publishedTopics;
+    private final TopicManagerSingleton.TopicManager topicManager;
+
+    public IncAgent(String[] subscribedTopics, String[] publishedTopics) {
+        this.subscribedTopics = subscribedTopics;
+        this.publishedTopics = publishedTopics;
+        this.topicManager = TopicManagerSingleton.get();
+
+        initializeSubscriptions();
+        initializePublications();
+    }
+
+    private void initializeSubscriptions() {
+        if (subscribedTopics.length > 0) {
+            topicManager.getTopic(subscribedTopics[0]).subscribe(this);
+        }
+    }
+
+    private void initializePublications() {
+        if (publishedTopics.length > 0) {
+            topicManager.getTopic(publishedTopics[0]).addPublisher(this);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public void reset() {
+        // No state to reset in this agent
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        if (Double.isNaN(msg.asDouble)) {
+            return;
+        }
+
+        if (hasPublishedTopics()) {
+            publishIncrementedValue(msg.asDouble);
+        }
+    }
+
+    private boolean hasPublishedTopics() {
+        return publishedTopics.length > 0;
+    }
+
+    private void publishIncrementedValue(double originalValue) {
+        double incrementedValue = originalValue + 1.0;
+        System.out.println(getName() + " Publishing result: " + incrementedValue + " to topic: " + publishedTopics[0]);
+        topicManager.getTopic(publishedTopics[0]).publish(new Message(incrementedValue));
+    }
+
+    @Override
+    public void close() {
+        unsubscribeFromTopics();
+        removeFromPublishers();
+    }
+
+    private void unsubscribeFromTopics() {
+        if (subscribedTopics.length > 0) {
+            topicManager.getTopic(subscribedTopics[0]).unsubscribe(this);
+        }
+    }
+
+    private void removeFromPublishers() {
+        if (publishedTopics.length > 0) {
+            topicManager.getTopic(publishedTopics[0]).removePublisher(this);
+        }
+    }
+}
---
+++ src/test/PlusAgent.java
@@ -0,0 +1,110 @@
+package test;
+
+import java.util.Arrays;
+
+public class PlusAgent implements Agent {
+
+    private final String[] subscribedTopics;
+    private final String[] publishedTopics;
+    private final TopicManagerSingleton.TopicManager topicManager;
+    private Double firstOperand;
+    private Double secondOperand;
+
+    public PlusAgent(String[] subscribedTopics, String[] publishedTopics) {
+        this.subscribedTopics = subscribedTopics;
+        this.publishedTopics = publishedTopics;
+        this.topicManager = TopicManagerSingleton.get();
+
+        initializeSubscriptions();
+        initializePublications();
+        resetOperands();
+    }
+
+    private void initializeSubscriptions() {
+        if (subscribedTopics.length > 0) {
+            topicManager.getTopic(subscribedTopics[0]).subscribe(this);
+        }
+        if (subscribedTopics.length > 1) {
+            topicManager.getTopic(subscribedTopics[1]).subscribe(this);
+        }
+    }
+
+    private void initializePublications() {
+        if (publishedTopics.length > 0) {
+            topicManager.getTopic(publishedTopics[0]).addPublisher(this);
+        }
+    }
+
+    private void resetOperands() {
+        this.firstOperand = 0.0;
+        this.secondOperand = 0.0;
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public void reset() {
+        resetOperands();
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        double messageValue = msg.asDouble;
+
+        if (Double.isNaN(messageValue)) {
+            return;
+        }
+
+        updateOperand(topic, messageValue);
+
+        if (areBothOperandsSet() && hasPublishedTopics()) {
+            publishResult();
+        }
+    }
+
+    private void updateOperand(String topic, double messageValue) {
+        if (subscribedTopics.length > 0 && topic.equals(subscribedTopics[0])) {
+            firstOperand = messageValue;
+        } else if (subscribedTopics.length > 1 && topic.equals(subscribedTopics[1])) {
+            secondOperand = messageValue;
+        }
+    }
+
+    private boolean areBothOperandsSet() {
+        return firstOperand != null && secondOperand != null;
+    }
+
+    private boolean hasPublishedTopics() {
+        return publishedTopics.length > 0;
+    }
+
+    private void publishResult() {
+        double result = firstOperand + secondOperand;
+        System.out.println(getName() + " Publishing result: " + result + " to topic: " + publishedTopics[0]);
+        topicManager.getTopic(publishedTopics[0]).publish(new Message(result));
+    }
+
+    @Override
+    public void close() {
+        unsubscribeFromTopics();
+        removeFromPublishers();
+    }
+
+    private void unsubscribeFromTopics() {
+        if (subscribedTopics.length > 0) {
+            topicManager.getTopic(subscribedTopics[0]).unsubscribe(this);
+        }
+        if (subscribedTopics.length > 1) {
+            topicManager.getTopic(subscribedTopics[1]).unsubscribe(this);
+        }
+    }
+
+    private void removeFromPublishers() {
+        if (publishedTopics.length > 0) {
+            topicManager.getTopic(publishedTopics[0]).removePublisher(this);
+        }
+    }
+}
