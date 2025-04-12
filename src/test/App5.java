168d2aed0b83249c1905fc2a961d5481bc835981
Ido Sar Shalom <idosarshalom@gmail.com>
2025-04-13 00:44:37 +0300

    wip

--- src/test/App5.java
+++ src/test/App5.java
@@ -6,13 +6,22 @@ import java.io.StringReader;
 
 public class App5 {
     public static void main(String[] args) {
-        String httpRequest = """
-                GET /api/resource?id=123&name=test HTTP/1.1
+        String httpRequest1 = """
+                GET /api/resource?id=123&name=test&ido=5&m=8 HTTP/1.1
                 Host: localhost
                 Content-Length: 11
                 
                 hello world
                 """;
+
+        String httpRequest = "GET /api/resource?id=123&name=test HTTP/1.1\n" +
+                "Host: example.com\n" +
+                "Content-Length: 5\n"+
+                "\n" +
+                "filename=\"hello_world.txt\"\n"+
+                "\n" +
+                "hello world!\n"+
+                "\n" ;
 
         try (BufferedReader reader = new BufferedReader(new StringReader(httpRequest))) {
             RequestParser.RequestInfo requestInfo = RequestParser.parseRequest(reader);
