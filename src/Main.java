76fd9b1389775a61e08b70abd1e2d23b60127c88
Ido Sar Shalom <idosarshalom@gmail.com>
2025-04-13 22:11:55 +0300

    the main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import graph.ConfLoader;
+import graph.TopicDisplayer;
+import server.HTTPServer;
+import server.MyHTTPServer;
+import servlets.HtmlLoader;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        HTTPServer server = new MyHTTPServer(8080, 5);
+        server.addServlet("GET", "/publish", new TopicDisplayer());
+        server.addServlet("POST", "/upload", new ConfLoader());
+        server.addServlet("GET", "/app/", new HtmlLoader("html_files"));
+        server.start();
+        System.in.read();
+        server.close();
+        System.out.println("done");
     }
 }
