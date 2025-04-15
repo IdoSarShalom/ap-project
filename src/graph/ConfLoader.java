083be9cc8a28a1fb6352d34ae23812035aac17b7
Ido Sar Shalom <idosarshalom@gmail.com>
2025-04-15 13:25:05 +0300

    we use graph.html to store graph's visualization

--- src/graph/ConfLoader.java
+++ src/graph/ConfLoader.java
@@ -12,7 +12,7 @@ import java.nio.charset.StandardCharsets;
 
 public class ConfLoader implements Servlet {
     private static final String TEMP_UPLOAD_PATH = "uploaded.conf";
-    private static final String GRAPH_HTML_PATH = "web/index.html";
+    private static final String GRAPH_HTML_PATH = "web/graph.html";
 
     @Override
     public void handle(RequestParser.RequestInfo requestInfo, OutputStream clientOutput) throws IOException {
