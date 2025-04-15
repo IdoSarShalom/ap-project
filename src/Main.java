ea5cff7a97cf4975ce6b5b0716cb731b89a3533e
Ido Sar Shalom <idosarshalom@gmail.com>
2025-04-16 00:08:45 +0300

    Adding the FaviconServlet for a favorite icon!

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import graph.EmptyLoader;
 import graph.TopicDisplayer;
 import server.HTTPServer;
 import server.MyHTTPServer;
+import servlets.FaviconServlet;
 import servlets.HtmlLoader;
 
 public class Main {
@@ -12,9 +13,10 @@ public class Main {
         server.addServlet("POST", "/upload", new ConfLoader());
         server.addServlet("POST", "/adam", new EmptyLoader());
         server.addServlet("GET", "/app/", new HtmlLoader("web"));
+        server.addServlet("GET", "/favicon.ico", new FaviconServlet());
         server.start();
         System.in.read();
         server.close();
-        System.out.println("done");
+        System.out.println("Server stopped.");
     }
 }
