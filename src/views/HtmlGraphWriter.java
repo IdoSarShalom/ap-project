d2aef54bae79b26370701c6021df36da24b9eb86
Ido Sar Shalom <idosarshalom@gmail.com>
2025-04-16 13:32:10 +0300

    organizing the GraphWriter

--- src/views/HtmlGraphWriter.java
+++ src/views/HtmlGraphWriter.java
@@ -12,21 +12,24 @@ import java.util.List;
 import java.util.Map;
 
 public class HtmlGraphWriter {
+    private static final String GRAPH_PATH = "web/graph.html";
 
     public static String getGraphHTML(Graph graph) throws IOException {
-        final String GRAPH_PATH = "web/graph.html";
-        String template = Files.readString(Paths.get(GRAPH_PATH));
+        String template = readTemplate();
         String jsonGraph = toGraphJson(graph);
 
         return template.replace("{{GRAPH_DATA}}", jsonGraph);
     }
 
-    private static String toGraphJson(Graph graph) {
-        List<Node> nodes = collectNodes(graph);
-        Map<Node, Integer> nodeIdMap = assignNodeIds(nodes);
-        List<Map<String, Object>> nodeData = buildNodeData(nodes, nodeIdMap);
-        List<Map<String, Object>> edgeData = buildEdgeData(nodes, nodeIdMap);
+    private static String readTemplate() throws IOException {
+        return Files.readString(Paths.get(GRAPH_PATH));
+    }
 
+    private static String toGraphJson(Graph graph) {
+        List<Node> nodes = getNodes(graph);
+        Map<Node, Integer> nodeIdMap = assignNodeIds(nodes);
+        List<Map<String, Object>> nodeData = createNodeData(nodes, nodeIdMap);
+        List<Map<String, Object>> edgeData = createEdgeData(nodes, nodeIdMap);
         Map<String, Object> elements = new HashMap<>();
         elements.put("nodes", nodeData);
         elements.put("edges", edgeData);
@@ -34,24 +37,23 @@ public class HtmlGraphWriter {
         return toJson(elements);
     }
 
-    private static List<Node> collectNodes(Graph graph) {
-        List<Node> nodes = new ArrayList<>();
-        for (Node node : graph) {
-            nodes.add(node);
-        }
-        return nodes;
+    private static List<Node> getNodes(Graph graph) {
+        return new ArrayList<>(graph);
     }
 
     private static Map<Node, Integer> assignNodeIds(List<Node> nodes) {
         Map<Node, Integer> nodeIdMap = new HashMap<>();
+
         for (int i = 0; i < nodes.size(); i++) {
             nodeIdMap.put(nodes.get(i), i + 1);
         }
+
         return nodeIdMap;
     }
 
-    private static List<Map<String, Object>> buildNodeData(List<Node> nodes, Map<Node, Integer> nodeIdMap) {
+    private static List<Map<String, Object>> createNodeData(List<Node> nodes, Map<Node, Integer> nodeIdMap) {
         List<Map<String, Object>> nodeData = new ArrayList<>();
+
         for (Node node : nodes) {
             Map<String, Object> nodeObj = new HashMap<>();
             int id = nodeIdMap.get(node);
@@ -59,13 +61,16 @@ public class HtmlGraphWriter {
             nodeObj.put("label", node.getName());
             nodeData.add(nodeObj);
         }
+
         return nodeData;
     }
 
-    private static List<Map<String, Object>> buildEdgeData(List<Node> nodes, Map<Node, Integer> nodeIdMap) {
+    private static List<Map<String, Object>> createEdgeData(List<Node> nodes, Map<Node, Integer> nodeIdMap) {
         List<Map<String, Object>> edgeData = new ArrayList<>();
+
         for (Node node : nodes) {
             int fromId = nodeIdMap.get(node);
+
             for (Node neighbor : node.getEdges()) {
                 int toId = nodeIdMap.get(neighbor);
                 Map<String, Object> edgeObj = new HashMap<>();
@@ -74,55 +79,65 @@ public class HtmlGraphWriter {
                 edgeData.add(edgeObj);
             }
         }
+
         return edgeData;
     }
 
     private static String toJson(Map<String, Object> map) {
         StringBuilder json = new StringBuilder();
         json.append("{");
-
         boolean first = true;
+
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             if (!first) {
                 json.append(",");
             }
             json.append("\"").append(entry.getKey()).append("\":");
-            json.append(toJson(entry.getValue()));
+            json.append(toJsonValue(entry.getValue()));
             first = false;
         }
-
         json.append("}");
+
         return json.toString();
     }
 
-    private static String toJson(Object value) {
+    private static String toJsonValue(Object value) {
+
         if (value instanceof Map) {
             return toJson((Map<String, Object>) value);
-        } else if (value instanceof List) {
-            return toJson((List<?>) value);
-        } else if (value instanceof String) {
-            return "\"" + escapeString((String) value) + "\"";
-        } else if (value instanceof Number || value instanceof Boolean) {
-            return value.toString();
-        } else {
-            return "null";
         }
+
+        if (value instanceof List) {
+            return toJsonList((List<?>) value);
+        }
+
+        if (value instanceof String) {
+            return "\"" + escapeString((String) value) + "\"";
+        }
+
+        if (value instanceof Number || value instanceof Boolean) {
+            return value.toString();
+        }
+
+        return "null";
     }
 
-    private static String toJson(List<?> list) {
+    private static String toJsonList(List<?> list) {
         StringBuilder json = new StringBuilder();
         json.append("[");
 
         boolean first = true;
+
         for (Object item : list) {
+
             if (!first) {
                 json.append(",");
             }
-            json.append(toJson(item));
+            json.append(toJsonValue(item));
             first = false;
         }
-
         json.append("]");
+
         return json.toString();
     }
 
